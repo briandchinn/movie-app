67262d3f38b2cdb1f57ff6126bba82feb2ee1b54
briandchinn <bchinn310@gmail.com>
2019-06-05 15:02:04 -0700

    adds bootstrap index.html page

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,6 @@
+class MoviesController < ApplicationController
+  def show
+    @movie = Movie.find(params[:id])
+    render "show.html.erb"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,6 @@ Rails.application.routes.draw do
 
   end
 
+  get "/movies/:id" => "movies#show"
+
 end
