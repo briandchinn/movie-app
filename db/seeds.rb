2b87440e68ebe45b211cdc70380a40b53cadbf1e
briandchinn <bchinn310@gmail.com>
2019-06-13 14:51:45 -0700

    adds updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,30 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-# 10.times do
-#      Actor.create(
-#         first_name: FFaker::Name.first_name,
-#         last_name: FFaker::Name.last_name,
-#         known_for: FFaker::Movie.title,
-#       ) 
-# end 
+User.create(name: "Brian", email: "brian@gmail.com", password_digest: "password")
+User.create(name: "Franny", email: "franny@gmail.com", password_digest: "password")
 
-5.times do
-  Movie.create(
-    title: FFaker::Movie.title,
-    year: FFaker::Vehicle.year,
-    plot: FFaker::BaconIpsum.sentence,
-    ) 
-end 
+
+Movie.create(title: "The Godfather", year: "1972", plot: "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.", director: "Francis Ford Coppola")
+Movie.create(title: "The Shawshank Redemption", year: "1994", plot: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.", director: "Frank Darabont")
+Movie.create(title: "Schindler's List", year: "1993", plot: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.", director: "Steven Spielberg")
+Movie.create(title: "Raging Bull", year: "1980", plot: "The life of boxer Jake LaMotta, who's violence and temper that lead him to the top in the ring destroyed his life outside of it.", director: "Martin Scorsese")
+Movie.create(title: "Casablanca", year: "1942", plot: "A cynical American expatriate struggles to decide whether or not he should help his former lover and her fugitive husband escape French Morocco.", director: "Michael Curtiz")
+
+
+Actor.create(first_name: "Morgan", last_name: "Freeman", gender: "Male", age: "72", known_for: "Se7en")
+Actor.create(first_name: "Leonardo", last_name: "DiCaprio", gender: "Male", age: "52", known_for: "Inception")
+Actor.create(first_name: "Natalie", last_name: "Portman", gender: "Female", age: "38", known_for: "Black Swan")
+
+
+Genre.create(name: "Drama")
+Genre.create(name: "Action")
+Genre.create(name: "Comedy")
+
+
+MovieGenre.create(genre_id: 1, movie_id: 1)
+MovieGenre.create(genre_id: 2, movie_id: 2)
+MovieGenre.create(genre_id: 3, movie_id: 3)
+MovieGenre.create(genre_id: 1, movie_id: 4)
+MovieGenre.create(genre_id: 2, movie_id: 5)
+
